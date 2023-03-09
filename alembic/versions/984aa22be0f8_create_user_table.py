3799004e3c4e68ede0acc3c952680236a2cf7a8c
czar <czar@MacBook-Pro-czar.local>
2023-03-09 10:16:09 +0400

    Added alembic to project

---
+++ alembic/versions/984aa22be0f8_create_user_table.py
@@ -0,0 +1,24 @@
+"""create user table
+
+Revision ID: 984aa22be0f8
+Revises: 
+Create Date: 2023-03-07 13:48:25.299166
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '984aa22be0f8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- app/database/db.py
+++ app/database/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, Float
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 # Create a database engine
